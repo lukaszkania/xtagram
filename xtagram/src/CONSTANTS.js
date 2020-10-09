d4efa7d3fb4095035da3aa37deb07da20c7a7b3b
Lukasz <lukasz.kania1996@gmail.com>
2020-10-09 15:41:23 +0200

    Added when post was added funcionality

---
+++ xtagram/src/CONSTANTS.js
@@ -0,0 +1,30 @@
+export const modifyDate = (dateFromApi) => {
+    let yearFromApiDate = parseInt(dateFromApi.slice(0,4));
+    let monthFromApiDate = parseInt(dateFromApi.slice(5,7));
+    let dayFromApiDate = parseInt(dateFromApi.slice(8,10));
+    let hourFromApiDate = parseInt(dateFromApi.slice(11,13)) + 2;
+    let minutesFromApiDate = parseInt(dateFromApi.slice(14,16));
+    
+    const timeNow = new Date();
+    let actualYear = timeNow.getFullYear();
+    let actualMonth = timeNow.getMonth() + 1;
+    let actualDay = timeNow.getDate();
+    let actualHour = timeNow.getHours();
+    let actualMinutes = timeNow.getMinutes();
+
+    if(actualYear === yearFromApiDate && actualMonth === monthFromApiDate && actualDay === dayFromApiDate){
+        if(actualHour === hourFromApiDate){
+            let howManyMinutesAgoPostedWasAdded = Math.abs(actualMinutes - minutesFromApiDate);
+            console.log(howManyMinutesAgoPostedWasAdded);
+            return (howManyMinutesAgoPostedWasAdded === 0 ? "Przed chwilą":`${howManyMinutesAgoPostedWasAdded} min temu`);
+        }else{
+            let howManyHoursAgoPostWasAdded = actualHour - hourFromApiDate;
+            return `${howManyHoursAgoPostWasAdded} h temu`;
+        }
+    }else if(actualYear === yearFromApiDate && actualMonth === monthFromApiDate){
+        let howManyDaysAgoPostWasAdded = actualDay - dayFromApiDate;
+        return `${howManyDaysAgoPostWasAdded} dni temu`
+    }else{
+        return `${dayFromApiDate}-${monthFromApiDate}-${dayFromApiDate}`;
+    }
+}
--- xtagram/src/Components/LoggedInUser/UnderPostBar/UnderPostBar.js
+++ xtagram/src/Components/LoggedInUser/UnderPostBar/UnderPostBar.js
@@ -3,26 +3,36 @@ import './UnderPostBar.scss';
 import axios from 'axios';
 import { LIKES_API, POSTS_API, USERS_API } from '../../../API_URLS';
 import { connect } from 'react-redux';
+import { modifyDate } from '../../../CONSTANTS';
 
 class UnderPostBar extends Component {
     state = { 
         isPostLikedByLoggedInUser: false,
-        usernamesOfUsersThatLikeThisPost: []
+        usernamesOfUsersThatLikeThisPost: [],
+        classNameOfHeartIcon: "far fa-heart",
+        likesAmount: 0,
+        postCreatedDate: ""
      }
 
     componentDidMount(){
         axios.get(this.props.postObject.likes_number).then(response => {
+            this.setState({
+                likesAmount: response.data.likes_number,
+                postCreatedDate: modifyDate(this.props.postObject.created_at)
+            })
             response.data.list_of_users_that_like_it.map(userUrl => {
                 axios.get(userUrl).then(response => {
+                    console.log(userUrl)
                     this.setState({
-                        usernamesOfUsersThatLikeThisPost: this.state.usernamesOfUsersThatLikeThisPost.concat([response.data.username])
+                        usernamesOfUsersThatLikeThisPost: this.state.usernamesOfUsersThatLikeThisPost.concat([response.data.username]),
                     })
                 }).catch(error => {
                     console.log(error.message)
                 })
                 if(userUrl === `${USERS_API}${this.props.idOfLoggedInUser}/`){
                     this.setState({
-                        isPostLikedByLoggedInUser: true
+                        isPostLikedByLoggedInUser: true,
+                        classNameOfHeartIcon: "far fa-heart liked"
                     })
                 }
             })
@@ -32,11 +42,13 @@ class UnderPostBar extends Component {
     }
 
     handleClickOnHeartIcon = event => {
-        event.preventDefault();
         let actualLikesAmount;
         let actualListOfUsersThatLikeIt;
         let urlOfLikesNumber;
         axios.get(`${POSTS_API}${this.props.postPk}/`).then(response => {
+            this.setState({
+
+            })
             urlOfLikesNumber = response.data.likes_number
         }).catch(error => {
             console.log(error.message)
@@ -49,13 +61,24 @@ class UnderPostBar extends Component {
             }).finally(()=> {
                 if(this.state.isPostLikedByLoggedInUser){
                     axios.put(urlOfLikesNumber, {
-                        likes_number: actualLikesAmount + -1,
+                        likes_number: actualLikesAmount -1,
                         list_of_users_that_like_it: actualListOfUsersThatLikeIt.filter(userUrl => {return userUrl !== `${USERS_API}${this.props.idOfLoggedInUser}/`})
+                    })
+                    this.setState({
+                        classNameOfHeartIcon:"far fa-heart",
+                        isPostLikedByLoggedInUser: false,
+                        likesAmount: this.state.likesAmount - 1
                     })
                 }else{
                     axios.put(urlOfLikesNumber, {
                         likes_number: actualLikesAmount + 1,
                         list_of_users_that_like_it: actualListOfUsersThatLikeIt.concat([`${USERS_API}${this.props.idOfLoggedInUser}/`])
+                        
+                    })
+                    this.setState({
+                        classNameOfHeartIcon:"far fa-heart liked",
+                        isPostLikedByLoggedInUser: true,
+                        likesAmount: this.state.likesAmount + 1
                     })}
             })
         })
@@ -65,18 +88,20 @@ class UnderPostBar extends Component {
         return ( 
             <div className="d-flex flex-column justify-content-around">
             <div className="under-post-bar-container d-flex d-flex align-items-center">
+                {this.state.test}
                 {this.state.isPostLikedByLoggedInUser ? 
                     (
-                        <i onClick={this.handleClickOnHeartIcon} className="far fa-heart liked"></i>
+                    <i onClick={this.handleClickOnHeartIcon} className={this.state.classNameOfHeartIcon}></i>
                     )
                     :
                     (
-                        <i onClick={this.handleClickOnHeartIcon} className="far fa-heart"></i>
+                        <i onClick={this.handleClickOnHeartIcon} className={this.state.classNameOfHeartIcon}></i>
                     )
                 }
             </div>
-            <div className="likes-number-container">Liczba polubień: {this.state.usernamesOfUsersThatLikeThisPost.length}</div>
+            <div className="likes-number-container">Liczba polubień: {this.state.likesAmount}</div>
             <div className="show-all-coments-container">Zobacz wszystkie komentarze</div>
+            <div className="post-date-container">Dodano {this.state.postCreatedDate}</div>
         </div>
          );
     }
